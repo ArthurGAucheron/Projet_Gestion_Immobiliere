9ab0150ee919261958ee47c3c29f00aaaea52c6d
Jeremie Billet <jeremiebillet@orange.fr>
2020-09-01 10:16:25 +0200

    add forms to app.module

--- front_end/frontGestionImmobiliere/src/app/app.module.ts
+++ front_end/frontGestionImmobiliere/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
@@ -50,7 +53,9 @@ import { ProprietaireCardComponent } from './composants/proprietaire/proprietair
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [
     AuthGuardService,{provide:HTTP_INTERCEPTORS, useClass:BasicAuthHttpInterceptorService, multi:true }
