790c3cc43003bf3ebb12b880a8045c1965618a9f
Camille Nguyen-Colas <c-amie@live.fr>
2020-09-01 18:33:47 +0200

    Getall OK

--- back_end/back_gestion_immobiliere/src/main/java/com/intiformation/gestion/immo/modele/ClasseStandard.java
+++ back_end/back_gestion_immobiliere/src/main/java/com/intiformation/gestion/immo/modele/ClasseStandard.java
@@ -1,11 +1,16 @@
 package com.intiformation.gestion.immo.modele;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Table(name = "classes")
@@ -29,6 +34,10 @@ public class ClasseStandard {
 
 	@Column(name = "superficie_min")
 	private double superficieMin;
+	
+	@OneToMany(targetEntity=BienImmobilier.class, mappedBy="classe")
+	@JsonBackReference
+	private List<BienImmobilier> biensImmobilier;
 
 	// ______________constructeurs______________
 
