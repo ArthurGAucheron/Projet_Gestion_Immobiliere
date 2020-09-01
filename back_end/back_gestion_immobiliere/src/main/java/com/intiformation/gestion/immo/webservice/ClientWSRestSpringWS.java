818b8527e821b564ecba003eb02cd76ff9a68c3d
marlene-legrand <marlene.legrand8@gmail.com>
2020-09-01 12:00:30 +0200

    suppression spring security

--- back_end/back_gestion_immobiliere/src/main/java/com/intiformation/gestion/immo/webservice/ClientWSRestSpringWS.java
+++ back_end/back_gestion_immobiliere/src/main/java/com/intiformation/gestion/immo/webservice/ClientWSRestSpringWS.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import com.intiformation.gestion.immo.modele.Client;
  * @author marle
  *
  */
+@CrossOrigin(value="http://localhost:8080")
 @RestController
 @RequestMapping(value="clients")
 public class ClientWSRestSpringWS {
