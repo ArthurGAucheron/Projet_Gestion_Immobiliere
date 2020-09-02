cd55bc71f5e32d2ac97a0236cee0052d9e1a170f
Jeremie Billet <jeremiebillet@orange.fr>
2020-09-02 09:54:14 +0200

    correction adresse

--- front_end/frontGestionImmobiliere/src/app/app-routing.module.ts
+++ front_end/frontGestionImmobiliere/src/app/app-routing.module.ts
@@ -8,18 +8,19 @@ import { ProprietaireListComponent } from './composants/proprietaire/proprietair
 import { ClientCreateComponent } from './composants/client/client-create/client-create.component';
 import { ClientListComponent } from './composants/client/client-list/client-list.component';
 import { ProprietaireCardComponent } from './composants/proprietaire/proprietaire-card/proprietaire-card.component';
+import { AuthGuardService } from './guards/auth-guard.service';
 
 const routes: Routes = [
 
   {path:"edit/contrat/:id", component:CreateContratComponent},
 
-  {path:"client_card/:id", component:ClientCardComponent, canActivate:[]},
-  {path:"client_edit/:id", component:ClientCreateComponent, canActivate:[]},
-  {path:"client_list", component:ClientListComponent, canActivate:[]},
+  {path:"client_card/:id", component:ClientCardComponent, canActivate:[AuthGuardService]},
+  {path:"client_edit/:id", component:ClientCreateComponent, canActivate:[AuthGuardService]},
+  {path:"client_list", component:ClientListComponent, canActivate:[AuthGuardService]},
 
-  {path:"proprietaire_card/:id", component:ProprietaireCardComponent, canActivate:[]},
-  {path:"proprietaire_edit/:id", component:ProprietaireCreateComponent, canActivate:[]},
-  {path:"proprietaire_list", component:ProprietaireListComponent, canActivate:[]},
+  {path:"proprietaire_card/:id", component:ProprietaireCardComponent, canActivate:[AuthGuardService]},
+  {path:"proprietaire_edit/:id", component:ProprietaireCreateComponent, canActivate:[AuthGuardService]},
+  {path:"proprietaire_list", component:ProprietaireListComponent, canActivate:[AuthGuardService]},
 ];
 
 @NgModule({
