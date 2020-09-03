1b463da072cdfced910d0acc1ceb4756be2e863a
giovanni <giovanni@mbp-de-giovanni.home>
2020-09-03 16:42:34 +0200

    modif contrats

--- back_end/back_gestion_immobiliere/src/main/java/com/intiformation/gestion/immo/dao/ContratRepository.java
+++ back_end/back_gestion_immobiliere/src/main/java/com/intiformation/gestion/immo/dao/ContratRepository.java
@@ -1,15 +1,10 @@
 package com.intiformation.gestion.immo.dao;
 
-import java.util.Optional;
-import java.util.Set;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
-import com.intiformation.gestion.immo.modele.BienImmobilier;
-import com.intiformation.gestion.immo.modele.ClasseStandard;
-import com.intiformation.gestion.immo.modele.ConseillerImmobilier;
 import com.intiformation.gestion.immo.modele.Contrat;
 
 /**
