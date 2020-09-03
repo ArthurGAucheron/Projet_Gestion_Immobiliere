d9f001a6328ee05cf04e78976e1a82487821a196
Art-GA <arthurgaucheron@hotmail.fr>
2020-09-03 11:45:31 +0200

    ajout liste visites et listes clients dans le modèle bienImmobilier

--- front_end/frontGestionImmobiliere/src/app/composants/bienImmobilier/liste-immobilier-achat/liste-immobilier.component.ts
+++ front_end/frontGestionImmobiliere/src/app/composants/bienImmobilier/liste-immobilier-achat/liste-immobilier.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { BienImmobilierService } from "src/app/services/bien-immobilier/bien-immobilier.service";
 import { Router } from '@angular/router';
 import { BienAchat } from 'src/app/modeles/BienAchat';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-liste-immobilier',
   templateUrl: './liste-immobilier.component.html',
   styleUrls: ['./liste-immobilier.component.css']
 })
+
+
+
 export class ListeImmobilierComponent implements OnInit {
 
   // ========== Propriétés ============
@@ -16,6 +22,8 @@ export class ListeImmobilierComponent implements OnInit {
   biensAchat = [];
   biensLocation = [];
   avendre ="à vendre";
+  dtOptions:DataTables.Settings={};
+  dtTrigger: Subject<any> = new Subject();
   
   // ========== Constructeurs ============
   constructor(private bienService : BienImmobilierService , private router : Router) { }
@@ -25,11 +33,21 @@ export class ListeImmobilierComponent implements OnInit {
   ngOnInit(): void {
     this.findAllAchatBien();
   }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
   findAllAchatBien(){
-    this.bienService.getAllAchat().subscribe(data => this.biens=data)
+    this.bienService.getAllAchat().subscribe(data => {this.biens=data;
+      this.dtTrigger.next();});
+    
   }// end findAll()
 
   consulterFiche(idBienImmo : number){
     this.router.navigate(['bienImmoAchat/fiche',idBienImmo]);
   }
+
+  
 }
